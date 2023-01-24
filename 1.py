ba84581303e05c0dbb9e4fa74f9a010296ed3855
nittalasyam <123496246+nittalasyam@users.noreply.github.com>
2023-01-24 23:32:10 +0530

    Update and rename README.md to 1.py

---
+++ 1.py
@@ -0,0 +1,16 @@
+print ("This is my first Python program")
+
+A = int(input("First Number: "))
+B = int(input("Second Number: "))
+C = A+B
+D = A-B
+E = A*B
+F = A/B
+
+print ("First Number:", A)
+print ("Second Number:", B)
+
+print ("A + B:", C)
+print ("A - B:", D)
+print ("A x B:", E)
+print ("A / B:", F)
